4d01e9443168d613f3e29dea92efb9c7fd39d476
Christian Vari <vari.christian@gmail.com>
2018-10-18 22:56:35 +0200

    All working but bad performance

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,19 @@
 from tictactoe_functions import *
+from sklearn.metrics import confusion_matrix
+
 
 DATA_PATH = "./ticTacToeDataset.txt"
 
 data = getData(DATA_PATH)
-#Divido training e test set
-train_set, test_set = doStratifiedShuffleSplit(data, data["Class"])
+doEncodeTextData(data)
+
+# Divido training e test set
+train_set, test_set, train_label_set, test_label_set = doStratifiedShuffleSplit(data, "Class", "Class")
+
+train_label_set = (train_label_set == 1)
+test_label_set = (test_label_set == 1)
+
+classificatore = createClassificator(train_set,train_label_set)
+predizioni = doPredictions(train_set, classificatore)
+
+print(confusion_matrix(predizioni, train_label_set))
--- tictactoe_functions.py
+++ tictactoe_functions.py
@@ -1,18 +1,43 @@
 import pandas as pd
 from os import path
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import SGDClassifier
+import numpy as np
 
 
-#Utility per caricare il csv
-def getData (data_path):
-	data_path = path.join(data_path)
-	return pd.read_csv(data_path)
+def getData(data_path):
+    data_path = path.join(data_path)
+    return pd.read_csv(data_path)
 
-def doStratifiedShuffleSplit(data ,stratifying_attribute):
-	split = StratifiedShuffleSplit(test_size = 0.2, random_state=42)
-	for train_index, test_index in split.split(data, stratifying_attribute):
-		strat_train_set = data.loc[train_index]
-		strat_test_set = data.loc[test_index]
 
-	return strat_train_set, strat_test_set
+def doStratifiedShuffleSplit(data, stratifying_attribute, label_attribute):
+    split = StratifiedShuffleSplit(test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(data, data[stratifying_attribute]):
+        strat_train_set = data.loc[train_index]
+        strat_test_set = data.loc[test_index]
+
+    train_label = strat_train_set[label_attribute]
+    test_label = strat_test_set[label_attribute]
+    strat_train_set = strat_train_set.drop(labels=label_attribute, axis=1)
+    strat_test_set = strat_test_set.drop(labels=label_attribute, axis=1)
+    print("Train: ", strat_train_set.shape, "label : ", train_label.shape, " Test: ", strat_test_set.shape, " label: ", test_label.shape)
+    return strat_train_set, strat_test_set, train_label, test_label
+
+
+def doEncodeTextData(data):
+    for c in data.columns:
+
+        encoder = LabelEncoder()
+        data[c] = encoder.fit_transform(data[c])
+
+
+def createClassificator(train, train_labels):
+    classificatore = SGDClassifier(random_state=42)
+    classificatore.fit(train, train_labels)
+    return classificatore
+
+
+def doPredictions(data, classificatore):
+    array = np.array(data)
+    return classificatore.predict(array)
